45ddadad7d3294954a8007f517e43c5368361fba
Brendan Moorhead <bmm112@pitt.edu>
2018-10-07 17:09:01 -0400

    logging in functionality

--- locker.py
+++ locker.py
@@ -1,37 +1,113 @@
+import os
 from flask import Flask, request, session, url_for, redirect, render_template, abort, g, flash
+from werkzeug import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+
+#from model import db, UserType, User, Scanner, Class, Item, Pattern, Alert, Messages
+from model import db, User
+
+
+SECRET_KEY = 'development key'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(app.root_path, 'model.db')
+
+app.config.from_object(__name__)
+
+db.init_app(app)
+
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Creates the database tables."""
+    db.create_all()
+    user = User.query.filter_by(username="owner").first()
+    if user is None:
+        db.session.add(
+            User(username='owner', password=generate_password_hash("pass"), firstName="Bob", lastName="Smith"))
+        db.session.commit()
+        user = None
+    print('Initialized the database.')
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user_id' in session:
+        g.user = User.query.filter_by(id=session['user_id']).first()
 
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=g.user)
 
 
-@app.route('/login')
+@app.route('/login/', methods=["GET", "POST"])
 def login():
+    if g.user:
+        return redirect(url_for('home'))
+
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user is None:
+            error = 'Invalid username'
+        elif not check_password_hash(user.password, request.form['password']):
+            error = 'Invalid password'
+        else:
+            flash('You were logged in')
+            session['user_id'] = user.id
+            return redirect(url_for('home'))
+
     return render_template("login.html")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Registers the user."""
+    error = None
+    if request.method == 'POST':
+        rv = User.query.filter_by(username=request.form['username']).first()
+        if not request.form['username']:
+            error = 'You have to enter a username'
+        elif not request.form['fName']:
+            error = 'You have to enter your first name'
+        elif not request.form['lName']:
+            error = 'You have to enter your last name'
+        elif not request.form['password']:
+            error = 'You have to enter a password'
+        elif request.form['password'] != request.form['password2']:
+            error = 'The two passwords do not match'
+        elif rv is not None:
+            error = 'The username is already taken'
+        else:
+            db.session.add(
+                User(username=request.form['username'], password=generate_password_hash(request.form['password']),
+                     firstName=request.form['fName'], lastName=request.form['lName']))
+            db.session.commit()
+            flash('You were successfully registered and can login now')
+            return redirect(url_for('login'))
+    return render_template('register.html')
 
 
 @app.route('/items')
 def items():
-    return render_template("items.html")
+    return render_template("items.html", user=g.user)
 
 
 @app.route('/status')
 def status():
-    return render_template("status.html")
+    return render_template("status.html", user=g.user)
 
 
 @app.route('/schedule')
 def schedule():
-    return render_template("schedule.html")
+    return render_template("schedule.html", user=g.user)
 
 
 @app.route('/notifications')
 def notifications():
-    return render_template("notifications.html")
+    return render_template("notifications.html", user=g.user)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -2,57 +2,105 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class UserType(db.Model):
-	ID = db.Column(db.Integer, primary_key=False)
-	userType = db.Column(db.String(24), nullable=False)
-	
+    ID = db.Column(db.Integer, primary_key=True)
+    userType = db.Column(db.String(24), nullable=False)
+
+    def __init__(self):
+        self.userType = ""
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(24), nullable=False)
-	password = db.Column(db.String(64), nullable=False)
-	firstName = db.Column(db.String(24), nullable=False)
-	lastName = db.Column(db.String(24), nullable=False)
-	userType = db.Column(db.Integer, db.ForeignKey("usertype.id"), nullable=False)
-	
+    password = db.Column(db.String(64), nullable=False)
+    firstName = db.Column(db.String(24), nullable=False)
+    lastName = db.Column(db.String(24), nullable=False)
+    # userType = db.Column(db.Integer, db.ForeignKey("usertype.id"), nullable=False)
+
+    def __init__(self, username, password, firstName, lastName):
+        self.username = username
+        self.password = password
+        self.firstName = firstName
+        self.lastName = lastName
+
+
 class Scanner(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	userID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-	scannerName = db.Column(db.String(64), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    scannerName = db.Column(db.String(64), nullable=False)
+
+    def __init__(self):
+        self.userID = 0
+        self.scannerName = ""
+
 
 class Class(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(24), nullable=False)
-	location = db.Column(db.String(24), nullable=False)
-	teacherID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-	StartTime = db.Column(db.DateTime, nullable=False)
-	EndTime = db.Column(db.DateTime, nullable=False)
-	
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(24), nullable=False)
+    location = db.Column(db.String(24), nullable=False)
+    teacherID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    startTime = db.Column(db.DateTime, nullable=False)
+    endTime = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self):
+        self.name = ""
+        self.location = ""
+        self.teacherID = 0
+        self.startTime = '1000-01-01 00:00:00.000000'
+        self.endTime = '1000-01-01 00:00:00.000000'
+
+
 class Item(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	tagID = db.Column(db.String(64), nullable=False)
-	name = db.Column(db.String(24), nullable=False)
-	classID = db.Column(db.Integer, db.ForeignKey("class.id"), nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    tagID = db.Column(db.String(64), nullable=False)
+    name = db.Column(db.String(24), nullable=False)
+    classID = db.Column(db.Integer, db.ForeignKey("class.id"), nullable=True)
+
+    def __init__(self):
+        self.tagID = ""
+        self.name = ""
+        self.classID = ""
+
 
 class Pattern(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	userID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-	itemID = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
-	dayOfWeek = db.Column(db.String(9), nullable=False)
-	startTime = db.Column(ARRAY[DateTime])
-	presentItem = db.Column(ARRAY[Integer])
-	
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    itemID = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    dayOfWeek = db.Column(db.String(9), nullable=False)
+    # startTime = db.Column(ARRAY[DateTime])
+    # presentItem = db.Column(ARRAY[Integer])
+
+    def __init__(self):
+        self.userID = 0
+        self.itemID = 0
+        self.dayOfWeek = ""
+
+
 class Alert(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	userID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-	itemID = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
-	message = db.Column(db.String(128), nullable=False)
-	time = db.Column(db.DateTime, nullable=False)
-	
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    itemID = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+    message = db.Column(db.String(128), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self):
+        self.userID = 0
+        self.itemID = 0
+        self.message = ""
+        self.time = '1000-01-01 00:00:00.000000'
+
+
 class Messages(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	userID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-	message = db.Column(db.String(128), nullable=False)
-	time = db.Column(db.DateTime, nullable=False)
-	itemID = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
-	
+    id = db.Column(db.Integer, primary_key=True)
+    userID = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    message = db.Column(db.String(128), nullable=False)
+    time = db.Column(db.DateTime, nullable=False)
+    itemID = db.Column(db.Integer, db.ForeignKey('item.id'), nullable=False)
+
+    def __init__(self):
+        self.userID = 0
+        self.message = ""
+        self.time = '1000-01-01 00:00:00.000000'
+        self.timeID = 0
